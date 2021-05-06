287a29d60c58665f337de6bc3cb4632e64372431
eastcoastdeveloper <uxdeveloper@protonmail.com>
2021-05-06 17:00:56 -0400

    1 file modified

--- src/app/doughnut-chart/doughnut-chart.component.ts
+++ src/app/doughnut-chart/doughnut-chart.component.ts
@@ -1,36 +1,36 @@
-import { Component, ElementRef, Input, OnInit, ViewChild } from "@angular/core";
-import { ChartType, Chart } from "chart.js";
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { ChartType, Chart } from 'chart.js';
 
 @Component({
-  selector: "doughnut-chart",
-  templateUrl: "./doughnut-chart.component.html",
-  styleUrls: ["./doughnut-chart.component.scss"]
+  selector: 'doughnut-chart',
+  templateUrl: './doughnut-chart.component.html',
+  styleUrls: ['./doughnut-chart.component.scss']
 })
 export class DoughnutChartComponent {
   chart: any;
-  @ViewChild("dChart", { static: false }) dChart: ElementRef;
+  @ViewChild('dChart', { static: false }) dChart: ElementRef;
   @Input() jsonArray: any = [25, 15, 20, 15, 30];
   @Input() chartLabels: any = [
-    "label 1",
-    "label 2",
-    "label 3",
-    "label 4",
-    "label 5"
+    'label 1',
+    'label 2',
+    'label 3',
+    'label 4',
+    'label 5'
   ];
   @Input() cutOut: number = 75;
   @Input() backgroundColors: any = [
-    "#E15D44",
-    "#55B4B0",
-    "#DFCFBE",
-    "#9B2335",
-    "#5B5EA6"
+    '#E15D44',
+    '#55B4B0',
+    '#DFCFBE',
+    '#9B2335',
+    '#5B5EA6'
   ];
 
   ngAfterViewInit() {
     let cvs: any;
     cvs = this.dChart.nativeElement;
     this.chart = new Chart(cvs, {
-      type: "doughnut",
+      type: 'doughnut',
       data: {
         labels: this.chartLabels,
         datasets: [
