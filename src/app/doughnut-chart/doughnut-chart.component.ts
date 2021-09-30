4165b77e907e4e037a8be5d7c50412389edc3de6
eastcoastdeveloper <uxdeveloper@protonmail.com>
2021-09-29 20:52:00 -0400

    1 file modified

--- src/app/doughnut-chart/doughnut-chart.component.ts
+++ src/app/doughnut-chart/doughnut-chart.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef,  Input,  OnInit, ViewChild } from '@angular/core';
 import { ChartType, Chart } from 'chart.js';
 
 @Component({
@@ -6,46 +6,34 @@ import { ChartType, Chart } from 'chart.js';
   templateUrl: './doughnut-chart.component.html',
   styleUrls: ['./doughnut-chart.component.scss']
 })
+
 export class DoughnutChartComponent {
-  chart: any;
-  @ViewChild('dChart', { static: false }) dChart: ElementRef;
-  @Input() jsonArray: any = [25, 15, 20, 15, 30];
-  @Input() chartLabels: any = [
-    'label 1',
-    'label 2',
-    'label 3',
-    'label 4',
-    'label 5'
-  ];
-  @Input() cutOut: number = 75;
-  @Input() backgroundColors: any = [
-    '#E15D44',
-    '#55B4B0',
-    '#DFCFBE',
-    '#9B2335',
-    '#5B5EA6'
-  ];
+
+  chart:any;
+  @ViewChild('dChart', {static:false}) dChart:ElementRef;
+  @Input() jsonArray:any = [25,15,20,15,30];
+  @Input() chartLabels:any = ["label 1", "label 2", "label 3", "label 4", "label 5"];
+  @Input() cutOut:number = 75;
+  @Input() backgroundColors: any = ['#E15D44', '#55B4B0', '#DFCFBE', '#9B2335', '#5B5EA6']
 
   ngAfterViewInit() {
-    let cvs: any;
-    cvs = this.dChart.nativeElement;
+    let cvs:any;
+        cvs = this.dChart.nativeElement;
     this.chart = new Chart(cvs, {
       type: 'doughnut',
       data: {
         labels: this.chartLabels,
-        datasets: [
-          {
-            data: this.jsonArray,
-            backgroundColor: this.backgroundColors,
-            fill: false,
-            borderWidth: 0
-          }
-        ]
+        datasets: [{
+          data: this.jsonArray,
+          backgroundColor: this.backgroundColors,
+          fill: false,
+          borderWidth: 0
+        }]
       },
       options: {
         responsive: false,
         legend: {
-          display: false
+          display: false,
         },
         cutoutPercentage: this.cutOut,
         tooltips: {
@@ -60,6 +48,6 @@ export class DoughnutChartComponent {
           }
         }
       }
-    });
+    })
   }
 }
