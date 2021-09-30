ffaf3851948c8b64284b47cb452faa8de7782a50
eastcoastdeveloper <uxdeveloper@protonmail.com>
2021-09-29 20:50:38 -0400

    6 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from "@angular/core";
+import { ChangeDetectorRef, Component, OnInit, ViewChild } from "@angular/core";
 import * as dataOne from "./dataOne.json";
 import * as dataTwo from "./dataTwo.json";
 import * as dataThree from "./dataThree.json";
@@ -9,6 +9,7 @@ import { DoughnutChartComponent } from "./doughnut-chart/doughnut-chart.componen
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"]
 })
+
 export class AppComponent {
   arrOne: any = dataOne.data[0];
   arrTwo: any = dataTwo.data[0];
@@ -17,4 +18,6 @@ export class AppComponent {
   @ViewChild(DoughnutChartComponent) dChartComp: any;
 
   doughnutCharts: any = [this.arrOne, this.arrTwo, this.arrThree];
+
 }
+
